a1683e8f26e3f097d86d46fe81eccd563733f78a
Venkataramana hegde <venkataramanahegde64@gmail.com>
2025-08-03 22:51:27 +0530

    Start arrays

---
+++ arrays/easy/linear_search.java
@@ -0,0 +1,23 @@
+package arrays.easy;
+
+class Solution {
+    public int linearSearch(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int target = 4;
+
+        Solution sol = new Solution();
+
+        int result = sol.linearSearch(nums, target);
+        
+        System.out.println(result);
+    }
+}
